210db426815edf64439f69131f47461f32530581
Grimfilerino <filip@funbutler.com>
2026-03-10 10:37:37 +0100

    added formatting for work stuff

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,13 +1,15 @@
 return {
     "nvimtools/none-ls.nvim",
-    cmd = "NoneLs",
     config = function()
         local null_ls = require("null-ls")
 
         null_ls.setup({
             sources = {
                 null_ls.builtins.formatting.stylua,
-                null_ls.builtins.formatting.clang_format
+                null_ls.builtins.formatting.clang_format,
+                null_ls.builtins.formatting.prettier.with({
+                    filetypes = { "html", "css", "scss", "typescript", "javascript" }
+                })
             },
         })
         vim.keymap.set('n', '<leader>zf', vim.lsp.buf.format, {})
